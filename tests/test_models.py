070a9ab3c7f1ea69d6d175e6644111e4048c2b11
Cunliang Geng <c.geng@esciencecenter.nl>
2024-10-14 16:20:21 +0200

    Create test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,26 @@
+import pytest
+from taxotagger import ProjectConfig
+from taxotagger.models import ModelFactory
+from taxotagger.models import MycoAIBERTEmbedModel
+from taxotagger.models import MycoAICNNEmbedModel
+from . import DATA_DIR
+
+
+@pytest.fixture
+def config():
+    return ProjectConfig(mycoai_home=str(DATA_DIR))
+
+
+def test_get_model_mycoai_cnn(config):
+    model = ModelFactory.get_model("MycoAI-CNN", config)
+    assert isinstance(model, MycoAICNNEmbedModel)
+
+
+def test_get_model_mycoai_bert(config):
+    model = ModelFactory.get_model("MycoAI-BERT", config)
+    assert isinstance(model, MycoAIBERTEmbedModel)
+
+
+def test_get_model_invalid(config):
+    with pytest.raises(ValueError, match="Invalid model id"):
+        ModelFactory.get_model("InvalidModel", config)
